717975cad7769910f5d5e1302d053cde6bb2d688
Gil Akos <akos.gil@gmail.com>
2016-11-20 14:44:02 -0600

    Initial Setup
    For testing - receiving errors on socket setup.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Require dependent libraries
 var express = require('express');
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
 var Typeform = require('typeform-node-api');
 
 // Define the port
@@ -37,31 +41,48 @@ When False, will skip the internal function
 */
 var local_test = true;
 
+// Testing Socket
+io.on('connection', function(socket){
+  console.log('socket connected');
+});
+
+/*
 // Request the typeform results with form id
 typeform_api.getCompletedFormResponses('mKCmta', function (data) {
-    if (local_test = true) {
-        // Drill to second level to the responses
-        var responses = data.responses;
-        // Create the initial obj format for scatter plot (graph 0)
-        var scatterData = {
+    // Drill to second level to the responses
+    var responses = data.responses;
+    // Create the initial obj format for scatter plot (graph 0)
+    var scatterData = {
             type: 'scatter plot',
             id: 0,
             data: []
         }
         // Loop through the responses
-        for (var i = 0; i < responses.length; i++) {
-            // Create empty obj for this response data point
-            var dataPoint = {};
-            // Drill down to answer level
-            var answer = responses[i].answers;
-            // Add properties for age, number of accounts, and gender
-            dataPoint.age = current_year - parseInt(answer.number_26064010);
-            dataPoint.numaccts = parseInt(answer.number_26116670);
-            dataPoint.gender = answer.listimage_26064008_choice;
-            // Add this data point to the scatter data object
-            scatterData.data.push(dataPoint);
-        }
+    for (var i = 0; i < responses.length; i++) {
+        // Create empty obj for this response data point
+        var dataPoint = {};
+        // Drill down to answer level
+        var answer = responses[i].answers;
+        // Add properties for age, number of accounts, and gender
+        dataPoint.age = current_year - parseInt(answer.number_26064010);
+        dataPoint.numaccts = parseInt(answer.number_26116670);
+        dataPoint.gender = answer.listimage_26064008_choice;
+        // Add this data point to the scatter data object
+        scatterData.data.push(dataPoint);
+    }
+    if (local_test == true) {
         // Log the result
         console.log(scatterData);
+    } else {
+        console.log('io socket connection goes here');
+        
+        io.on('connection', function (socket) {
+            socket.on('graph_data_0', function (scatterData) {
+                io.emit('graph_data_0', scatterData);
+            });
+        });
+        
     }
 });
+
+*/
