0011ba0fa33d81d48f7ea75fc2541f71542eb23d
Gil Akos <gil@astra.finance>
2016-12-03 22:16:44 -0600

    SocketIO working
    Socket io connection to front end is working now.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // Require dependent libraries
 var Typeform = require('typeform-node-api');
 
-// Setup basic express server
+// Require express
 var express = require('express');
+//Express initializes app to be a function handler that you can supply to an HTTP server.
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+//Create an HTTP server.
+var http = require('http').Server(app);
+//Create instance of socket
+var io = require('socket.io')(http);
+
 // Define the port
-var port = process.env.PORT || 3000;
+var port = 3000;
+//var port = process.env.PORT || 3000;
 
 // Send initial console message
-server.listen(port, function () {
+http.listen(3000, function () {
     console.log('Server listening at port %d', port);
 });
 
@@ -26,10 +31,10 @@ app.get('/', function (request, response) {
     response.render('pages/index');
 });
 
-// Write listen message
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
+//// Write listen message
+//app.listen(app.get('port'), function () {
+//    console.log('Node app is running on port', app.get('port'));
+//});
 
 // Create new typeform api object with our api key
 var typeform_api = new Typeform('3b6a02cd52c56c8a06df60d75979178c619d7f1e');
@@ -51,42 +56,39 @@ var local_test = false;
 //    console.log('socket connected');
 //});
 
+//On socket connection, pull from typeform
+io.on('connection', function (socket) {
 
-// Request the typeform results with form id
-typeform_api.getCompletedFormResponses('mKCmta', function (data) {
-    // Drill to second level to the responses
-    var responses = data.responses;
-    // Create the initial obj format for scatter plot (graph 0)
-    var scatterData = {
-            type: 'scatter plot',
-            id: 0,
-            data: []
+    // Request the typeform results with form id
+    typeform_api.getCompletedFormResponses('mKCmta', function (data) {
+        // Drill to second level to the responses
+        var responses = data.responses;
+        // Create the initial obj format for scatter plot (graph 0)
+        var scatterData = {
+                type: 'scatter plot',
+                id: 0,
+                data: []
+            }
+            // Loop through the responses
+        for (var i = 0; i < responses.length; i++) {
+            // Create empty obj for this response data point
+            var dataPoint = {};
+            // Drill down to answer level
+            var answer = responses[i].answers;
+            // Add properties for age, number of accounts, and gender
+            dataPoint.age = current_year - parseInt(answer.number_26064010);
+            dataPoint.numaccts = parseInt(answer.number_26116670);
+            dataPoint.gender = answer.listimage_26064008_choice;
+            // Add this data point to the scatter data object
+            scatterData.data.push(dataPoint);
         }
-        // Loop through the responses
-    for (var i = 0; i < responses.length; i++) {
-        // Create empty obj for this response data point
-        var dataPoint = {};
-        // Drill down to answer level
-        var answer = responses[i].answers;
-        // Add properties for age, number of accounts, and gender
-        dataPoint.age = current_year - parseInt(answer.number_26064010);
-        dataPoint.numaccts = parseInt(answer.number_26116670);
-        dataPoint.gender = answer.listimage_26064008_choice;
-        // Add this data point to the scatter data object
-        scatterData.data.push(dataPoint);
-    }
-    if (local_test == true) {
-        // Log the result
-        console.log(scatterData);
-    } else {
-        console.log('io socket connection goes here');
-        
-        io.on('connection', function (socket) {
-            socket.on('graph_data_0', function (scatterData) {
-                io.emit('graph_data_0', scatterData);
-            });
-        });
-        
-    }
+        if (local_test == true) {
+            // Log the result
+            console.log(scatterData);
+        } else {
+            console.log('io socket connection goes here');
+            //emit the message to all sockets, including the sender
+            io.emit('graph_data_0', scatterData);
+        }
+    });
 });
-
