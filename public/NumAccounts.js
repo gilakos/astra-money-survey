a314aaf1778bc1f6e8a774c416305163bbe2ef9f
Ben Epstein <ben.epstein97@gmail.com>
2016-12-23 22:08:33 -0500

    made graph zoom friendly with hover for details

--- public/NumAccounts.js
+++ public/NumAccounts.js
@@ -1,100 +1,174 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = { top: 50, right: 300, bottom: 50, left: 50 },
+    outerWidth = 1300,
+    outerHeight = 500,
+    width = outerWidth - margin.left - margin.right,
+    height = outerHeight - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
-    .range([0, width]);
+    .range([0, width]).nice();
 
 var y = d3.scale.linear()
-    .range([height, 0]);
+    .range([height, 0]).nice();
 
-var color = d3.scale.category10();
+var xCat = "age",
+    yCat = "numaccts";
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// d3.json("views/data.json", function(error, data) {
 var socket = io();
     socket.on('graph_data_0', function(data, error) {
-    console.log('in numaccounts');
-    // console.log(data.data[1].gender);
+  data.data.forEach(function(d) {
+    d.age = +d.age;
+    gender = +d.gender;     //not sure why this one can't be d.gender, but it won't recognize later if it is
+    d.numaccts = +d.numaccts;
+  });
 
-    if (error) throw error;
+  var xMax = d3.max(data.data, function(d) { return d[xCat]; }) * 1.05,
+      xMin = d3.min(data.data, function(d) { return d[xCat]; }),
+      xMin = xMin > 0 ? 0 : xMin,
+      yMax = d3.max(data.data, function(d) { return d[yCat]; }) * 1.05,
+      yMin = d3.min(data.data, function(d) { return d[yCat]; }),
+      yMin = yMin > 0 ? 0 : yMin;
 
-    data.data.forEach(function(d) {
-        d.age = +d.age;
-        gender = +d.gender;     //not sure why this one can't be d.gender, but it won't recognize later if it is
-        d.numaccts = +d.numaccts;
-    });
+  x.domain([xMin, xMax]);
+  y.domain([yMin, yMax]);
 
-    x.domain(d3.extent(data.data, function(d) { return d.age; })).nice();
-    y.domain(d3.extent(data.data, function(d) { return d.numaccts; })).nice();
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickSize(-height);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .append("text")
-        .attr("class", "label")
-        .attr("x", width)
-        .attr("y", -6)
-        .style("text-anchor", "end")
-        .text("Age");
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .tickSize(-width);
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("class", "label")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Num Accounts")
+  var color = d3.scale.category10();
 
-    svg.selectAll(".dot")
-        .data(data.data)
-        .enter().append("circle")
-        .attr("class", "dot")
-        .attr("r", 3.5)
-        .attr("cx", function(d) { return x(d.age); })
-        .attr("cy", function(d) { return y(d.numaccts); })
-        .style("fill", function(d){
+  var tip = d3.tip()
+      .attr("class", "d3-tip")
+      .offset([-10, 0])
+      .html(function(d) {
+        return xCat + ": " + d[xCat] + "<br>" + yCat + ": " + d[yCat];
+      });
+
+  var zoomBeh = d3.behavior.zoom()
+      .x(x)
+      .y(y)
+      .scaleExtent([0, 500])
+      .on("zoom", zoom);
+
+  var svg = d3.select("#NumAccounts")
+    .append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .call(zoomBeh);
+
+  svg.call(tip);
+
+  svg.append("rect")
+      .attr("width", width)
+      .attr("height", height);
+
+  svg.append("g")
+      .classed("x axis", true)
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .classed("label", true)
+      .attr("x", width)
+      .attr("y", margin.bottom - 10)
+      .style("text-anchor", "end")
+      .text("Age");
+
+  svg.append("g")
+      .classed("y axis", true)
+      .call(yAxis)
+    .append("text")
+      .classed("label", true)
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Num Accounts");
+
+  var objects = svg.append("svg")
+      .classed("objects", true)
+      .attr("width", width)
+      .attr("height", height);
+
+  objects.append("svg:line")
+      .classed("axisLine hAxisLine", true)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", width)
+      .attr("y2", 0)
+      .attr("transform", "translate(0," + height + ")");
+
+  objects.append("svg:line")
+      .classed("axisLine vAxisLine", true)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", 0)
+      .attr("y2", height);
+
+  objects.selectAll(".dot")
+      .data(data.data)
+    .enter().append("circle")
+      .classed("dot", true)
+      .attr("r", 6)
+      .attr("transform", transform)
+      .style("fill", function(d){
             if(d.gender == "Female") {  //if the gender var above is set as d.gender, this returns "NAN", not sure why
                 return "hotpink"; 
-            }
-            else 
-                return "blue";
-        })
-        // .style("fill", "blue")
+        }
+        else 
+            return "blue";
+    })
+      .on("mouseover", tip.show)
+      .on("mouseout", tip.hide);
 
-    var legend = svg.selectAll(".legend")
-        .data(color.domain())
-        .enter().append("g")
-        .attr("class", "legend")
-        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+  var legend = svg.selectAll(".legend")
+      .data(color.domain())
+    .enter().append("g")
+      .classed("legend", true)
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-    legend.append("rect")
-        .attr("x", width - 18)
-        .attr("width", 18)
-        .attr("height", 18)
-        .style("fill", color);
+  legend.append("circle")
+      .attr("r", 6)
+      .attr("cx", width + 20)
+      .attr("fill", color);
 
-    legend.append("text")
-        .attr("x", width - 24)
-        .attr("y", 9)
-        .attr("dy", ".35em")
-        .style("text-anchor", "end")
-        .text(function(d) { return d; });
+  legend.append("text")
+      .attr("x", width + 26)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
+
+  d3.select("input").on("click", change);
+
+  function change() {
+    xCat = "age";
+    xMax = d3.max(data.data, function(d) { return d[xCat]; });
+    xMin = d3.min(data.data, function(d) { return d[xCat]; });
+
+    zoomBeh.x(x.domain([xMin, xMax])).y(y.domain([yMin, yMax]));
+
+    var svg = d3.select("#NumAccounts").transition();
+
+    svg.select(".x.axis").duration(750).call(xAxis).select(".label").text(xCat);
+
+    objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
+  }
+
+  function zoom() {
+    svg.select(".x.axis").call(xAxis);
+    svg.select(".y.axis").call(yAxis);
+
+    svg.selectAll(".dot")
+        .attr("transform", transform);
+  }
+
+  function transform(d) {
+    return "translate(" + x(d[xCat]) + "," + y(d[yCat]) + ")";
+  }
 });
