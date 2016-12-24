14fb9bd64a2d028acc2f72d09c131722e55d0a9f
Ben Epstein <ben.epstein97@gmail.com>
2016-12-23 22:54:31 -0500

    added comments

--- public/NumAccounts.js
+++ public/NumAccounts.js
@@ -1,5 +1,5 @@
 var margin = { top: 50, right: 300, bottom: 50, left: 50 },
-    outerWidth = 1300,
+    outerWidth = 1300,  //should check width and height for all screens, maybe use %?
     outerHeight = 500,
     width = outerWidth - margin.left - margin.right,
     height = outerHeight - margin.top - margin.bottom;
@@ -14,21 +14,21 @@ var xCat = "age",
     yCat = "numaccts";
 
 var socket = io();
-    socket.on('graph_data_0', function(data, error) {
-  data.data.forEach(function(d) {
-    d.age = +d.age;
+    socket.on('graph_data_0', function(data, error) {   //grabs live typeform data
+  data.data.forEach(function(d) {  //data.data is used everywhere because 'data' is the object name, with many children nodes, 
+    d.age = +d.age;                    //one of which is (inconveniently) called 'data', which is the object we need to work with
     gender = +d.gender;     //not sure why this one can't be d.gender, but it won't recognize later if it is
     d.numaccts = +d.numaccts;
   });
 
-  var xMax = d3.max(data.data, function(d) { return d[xCat]; }) * 1.05,
+  var xMax = d3.max(data.data, function(d) { return d[xCat]; }) * 1.05,  //xCat is simply "age" in object, 1.05 may need to be removed
       xMin = d3.min(data.data, function(d) { return d[xCat]; }),
       xMin = xMin > 0 ? 0 : xMin,
       yMax = d3.max(data.data, function(d) { return d[yCat]; }) * 1.05,
       yMin = d3.min(data.data, function(d) { return d[yCat]; }),
       yMin = yMin > 0 ? 0 : yMin;
 
-  x.domain([xMin, xMax]);
+  x.domain([xMin, xMax]);  //this may not be necessary
   y.domain([yMin, yMax]);
 
   var xAxis = d3.svg.axis()
@@ -43,28 +43,28 @@ var socket = io();
 
   var color = d3.scale.category10();
 
-  var tip = d3.tip()
+  var tip = d3.tip()     //tip, when used, is for the hover that shows the values of each node
       .attr("class", "d3-tip")
       .offset([-10, 0])
       .html(function(d) {
         return xCat + ": " + d[xCat] + "<br>" + yCat + ": " + d[yCat];
       });
 
-  var zoomBeh = d3.behavior.zoom()
+  var zoomBeh = d3.behavior.zoom()    //for scaling the zoom correctly 
       .x(x)
       .y(y)
       .scaleExtent([0, 500])
       .on("zoom", zoom);
 
-  var svg = d3.select("#NumAccounts")
-    .append("svg")
+  var svg = d3.select("#NumAccounts")  //adding the svg (d3) body to the NumAccounts id on index.html
+    .append("svg")                     //this allows the svg body to be dynamic when zooming, not creating a static svg body
       .attr("width", outerWidth)
       .attr("height", outerHeight)
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")") 
       .call(zoomBeh);
 
-  svg.call(tip);
+  svg.call(tip); 
 
   svg.append("rect")
       .attr("width", width)
@@ -118,7 +118,7 @@ var socket = io();
       .classed("dot", true)
       .attr("r", 6)
       .attr("transform", transform)
-      .style("fill", function(d){
+      .style("fill", function(d){       //setting the color for man vs woman
             if(d.gender == "Female") {  //if the gender var above is set as d.gender, this returns "NAN", not sure why
                 return "hotpink"; 
         }
@@ -128,7 +128,7 @@ var socket = io();
       .on("mouseover", tip.show)
       .on("mouseout", tip.hide);
 
-  var legend = svg.selectAll(".legend")
+  var legend = svg.selectAll(".legend")   //for a key on the right end, may not be necessary for us.
       .data(color.domain())
     .enter().append("g")
       .classed("legend", true)
@@ -146,7 +146,7 @@ var socket = io();
 
   d3.select("input").on("click", change);
 
-  function change() {
+  function change() {   //allows the graph to be dynamic
     xCat = "age";
     xMax = d3.max(data.data, function(d) { return d[xCat]; });
     xMin = d3.min(data.data, function(d) { return d[xCat]; });
@@ -160,7 +160,7 @@ var socket = io();
     objects.selectAll(".dot").transition().duration(1000).attr("transform", transform);
   }
 
-  function zoom() {
+  function zoom() {  
     svg.select(".x.axis").call(xAxis);
     svg.select(".y.axis").call(yAxis);
 
