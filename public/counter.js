9555cd13d46b0c3e0d47dca29d16b13680a53271
Ben Epstein <ben.epstein97@gmail.com>
2017-02-17 18:28:29 -0600

    added wordcloud with final mods

--- public/counter.js
+++ public/counter.js
@@ -4,7 +4,7 @@ function wordCloud(selector) {
 
     //Construct the word cloud's SVG element
     var svg = d3.select(selector).append("svg")
-        .attr("width", 500)
+        .attr("width", 750)
         .attr("height", 500)
         .append("g")
         .attr("transform", "translate(250,250)");
@@ -15,7 +15,7 @@ function wordCloud(selector) {
         var cloud = svg.selectAll("g text")
             .data(words, function (d) {
                 return d.text;
-            })
+            });
 
         //Entering words
         cloud.enter()
@@ -79,43 +79,90 @@ function wordCloud(selector) {
 }
 
 //http://bl.ocks.org/jwhitfieldseed/9697914
+// var words = new Hashtable();
+var words1 = [];
 var words = [];
+var wordFreq = [];
 var temp = [];
 var socket2 = io();
 var word = "";
-var badWords = ["NA", "N/A", "no", "nope", "na", "n/a", "and", "or", "app", "apps", "a", "ie", "none", "don't",
-                "dont", "&", "my", "for", "each", "my"];
-for(var i = 0; i<badWords.length; i++){
-    badWords[i] = badWords[i].toUpperCase();
+var badWords = ["NA", "N/A", "no", "nope", "na", "n/a", "and", "or", "app", "apps", "a", "ie", "none", "don't", "so", "america",
+    "dont", "&", "my", "for", "each", "my", "I", "of", "to", "well", "like", "do", "now", "com", "", "use", "one", "hood", "just", "that", "bank's",
+    "(ie", "ever", "only", "but", "i've", "tried", "you", "but", "of", "have", "'s", "fig", "which", "mostly"];
+for (var i = 0; i < badWords.length; i++) {
+    badWords[i] = badWords[i].toLowerCase();
 }
-console.log(badWords);
-
+var exists = false;
 socket2.on('graph_data_0', function (data, error) {   //grabs live typeform data
     data.data.forEach(function (d) {  //data.data is used everywhere because 'data' is the object name, with many children nodes,
-        if (typeof d.appsUsed != 'undefined' && badWords.indexOf(d.appsUsed.toUpperCase()) < 0) { //dont want words in the badWords array
-            temp = d.appsUsed.split(/[ ,.]+/);
-            for(var i = 0; i < temp.length; i++){
-                if(badWords.indexOf(temp[i].toUpperCase()) < 0)
-                words.push(temp[i]);
+        if (typeof d.appsUsed != 'undefined' && badWords.indexOf(d.appsUsed.toLowerCase()) < 0) { //dont want words in the badWords array
+            temp = d.appsUsed.split(/[ ,.)(]+/);
+            for (var i = 0; i < temp.length; i++) {
+                if (badWords.indexOf(temp[i].toLowerCase()) < 0) {
+                    for (var j = 0; j < words1.length; j++) {
+                        if (words1[j].key.toLowerCase() == temp[i].toLowerCase()) {
+                            words1[j].value = words1[j].value + 1;
+                            exists = true;
+                        }
+                    }
+                    if (!exists) {
+                        words1.push({
+                            key: temp[i].toLowerCase(),
+                            value: 1
+                        });
+                    }
+                    exists = false;
+                }
             }
-        // .split(/[ ,]+/)
+            // .split(/[ ,]+/)
         }
+
     });
+    for (var i = 0; i < words1.length; i++) {
+        if (words1[i].value >= 1) {
+            if (words1[i].key == "capital") {
+                words.push("capital one");
+                wordFreq.push(words1[i].value);
+            }
+            if (words1[i].key == "robin") {
+                words.push("robinhood");
+                wordFreq.push(words1[i].value);
+            }
+            if (words1[i].key == "merrill") {
+                words.push("merrill lynch");
+                wordFreq.push(words1[i].value);
+            }
+            if (words1[i].key == "sig") {
+                words.push("sig fig");
+                wordFreq.push(words1[i].value);
+            }
+            if (words1[i].key == "wells") {
+                words.push("wells fargo");
+                wordFreq.push(words1[i].value);
+            }
+            words.push(words1[i].key);
+            wordFreq.push(words1[i].value);
+        }
+    }
+
     console.log(words);
-    // for(var i = 0; i < words.length; i++){
-    //     word += words[i];
-    // }
-    // words.remo
+    console.log(wordFreq);
 
 //Prepare one of the sample sentences by removing punctuation,
 // creating an array of words and computing a random size attribute.
     function getWords(i) {
-        return words[i]
-            .replace(/[!\.,:;()\?]/g, '')
-            .split(' ')
-            .map(function (d) {
-                return {text: d, size: 10 + Math.random() * 60};
-            })
+
+        var bagowords = [];
+        for (var j = 0; j < words.length; j++) {
+            bagowords.push({text: words[j], size: wordFreq[j] * 10})
+        }
+        return bagowords
+        // var count = 0;
+        // return words
+        //     .map(function (d) {
+        //         console.log(wordFreq[count++]);
+        //         return {text: d, size: wordFreq[count++]*5};
+        //     })
     }
 
 //This method tells the word cloud to redraw with a new set of words.
@@ -123,10 +170,10 @@ socket2.on('graph_data_0', function (data, error) {   //grabs live typeform data
 // user input or some other source.
     function showNewWords(vis, i) {
         i = i || 0;
-        vis.update(getWords(i++ % words.length))
-        setTimeout(function () {
-            showNewWords(vis, i + 1)
-        }, 2000)
+        vis.update(getWords(i++ % words.length));
+        // setTimeout(function () {
+        //     showNewWords(vis, i + 1)
+        // }, 2000)
     }
 
 //Create a new instance of the word cloud visualisation.
@@ -135,4 +182,5 @@ socket2.on('graph_data_0', function (data, error) {   //grabs live typeform data
 //Start cycling through the demo data
     showNewWords(myWordCloud);
 
-});
+})
+;
